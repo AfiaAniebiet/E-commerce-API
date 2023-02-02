6a115c5bee29c53e05054ef18b6bc66c34956751
aniebietafia <aniebietafia87@gmail.com>
2023-02-02 20:01:41 +0100

    Added Register functionality

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,19 +1,28 @@
-const User = require('../models/user.model')
+const User = require("../models/user.model");
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
 
 const registerUser = async (req, res, next) => {
-    res.send('Register Route')
-}
+  const { email } = req.body;
+  const emailExists = await User.findOne({ email });
+
+  if (emailExists) {
+    throw new CustomError.BadRequestError("Email already exists");
+  }
+  const user = await User.create(req.body);
+  res.status(StatusCodes.CREATED).json({ user });
+};
 
 const loginUser = async (req, res, next) => {
-    res.send('Login Route')
-}
+  res.send("Login Route");
+};
 
 const logoutUser = async (req, res, next) => {
-    res.send('Logout Route')
-}
+  res.send("Logout Route");
+};
 
 module.exports = {
-    registerUser,
-    loginUser,
-    logoutUser
-}
+  registerUser,
+  loginUser,
+  logoutUser,
+};

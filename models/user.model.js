1172d94a74694d12e3e1d58e281ffa5dd5503c4e
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-02-06 22:14:12 +0100

    Updated with middleware for password hashing

--- models/user.model.js
+++ models/user.model.js
@@ -1,30 +1,43 @@
-const mongoose = require('mongoose')
-const validator = require('validator')
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     fullName: {
-        type: String,
-        required: [true, "Please provide your full name."]
+      type: String,
+      required: [true, "Please provide your full name."],
     },
     email: {
-        type: String,
-        required: [true, "Please provide an email."],
-        unique: true,
-        validate: {
-            validator: validator.isEmail,
-            message: 'Please provide a valid email.'
-        }
+      type: String,
+      required: [true, "Please provide an email."],
+      unique: true,
+      validate: {
+        validator: validator.isEmail,
+        message: "Please provide a valid email.",
+      },
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     role: {
-        type: String,
-        enums: ['Admin', 'User'],
-        default: 'Admin'
-    }
+      type: String,
+      enums: ["admin", "user"],
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
 
-}, {timestamps: true})
+userSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(12);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
-module.exports = mongoose.model('User', userSchema)
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  const isMatch = await bcrypt.compare(enteredPassword, this.password);
+  return isMatch;
+};
+
+module.exports = mongoose.model("User", userSchema);

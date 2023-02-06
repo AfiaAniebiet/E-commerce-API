475663edd1cef6d71a79d3f5198ae6eebeb10d3b
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-02-06 22:15:21 +0100

    [New Feature]Added Login and Logout middleware

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,24 +1,60 @@
 const User = require("../models/user.model");
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
+const jwt = require("jsonwebtoken");
+const { attachCookiesToResponse } = require("../utils/index");
 
+// Register new user
 const registerUser = async (req, res, next) => {
-  const { email } = req.body;
+  const { email, fullName, password } = req.body;
   const emailExists = await User.findOne({ email });
 
   if (emailExists) {
     throw new CustomError.BadRequestError("Email already exists");
   }
-  const user = await User.create(req.body);
-  res.status(StatusCodes.CREATED).json({ user });
+
+  //   First registered account is an admin
+  const isFirstAccount = (await User.countDocuments({})) === 0;
+  const role = isFirstAccount ? "admin" : "user";
+
+  const user = await User.create({ email, fullName, password, role });
+  const tokenUser = { fullName: user.fullName, userId: user._id, email: user.email, role: user.role };
+
+  attachCookiesToResponse({ res, user: tokenUser });
+
+  res.status(StatusCodes.CREATED).json({ user: tokenUser });
 };
 
+// Login user functinality
 const loginUser = async (req, res, next) => {
-  res.send("Login Route");
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new CustomError.BadRequestError("Provide email and password");
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new CustomError.UnauthenticatedError("User does not exist.");
+  }
+
+  const isPassword = await user.comparePassword(password);
+  if (!isPassword) {
+    throw new CustomError.UnauthenticatedError("User does not exist.");
+  }
+
+  const tokenUser = { fullName: user.fullName, userId: user._id, email: user.email, role: user.role };
+
+  attachCookiesToResponse({ res, user: tokenUser });
+
+  res.status(StatusCodes.OK).json({ user: tokenUser });
 };
 
+// Logout signed in user
 const logoutUser = async (req, res, next) => {
-  res.send("Logout Route");
+  res.cookie("token", "logout", {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
 };
 
 module.exports = {

af17582edb34c574b9aca281de8ab2a1aef3e5cd
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-02-07 22:01:12 +0100

    Added authentication to all the routes

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const userController = require("../controllers/user.controller");
+const { authUser, authorizePermissions } = require("../middleware/authentication");
 
-router.route("/").get(userController.getAllUsers);
-router.route("/currentUser").get(userController.showCurrentUser);
-router.route("/updateUser").patch(userController.updateUser);
-router.route("/updatePassword").patch(userController.updateUserPassword);
-router.route("/:id").get(userController.getSingleUser);
+router.route("/").get(authUser, authorizePermissions("admin", "owner"), userController.getAllUsers);
+router.route("/currentUser").get(authUser, userController.showCurrentUser);
+router.route("/updateUser").patch(authUser, userController.updateUser);
+router.route("/updatePassword").patch(authUser, userController.updateUserPassword);
+router.route("/:id").get(authUser, userController.getSingleUser);
 
 module.exports = router;

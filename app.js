add4c16f4409a117b9c216e56aca25568ba0e01b
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-02-10 13:40:51 +0100

    Added upload: cannot open `upload' (No such file or directory) middleware

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 require("express-async-errors");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
 
 //Database connection
 const MONGODB_CONNECTION = require("./database/database");
@@ -12,6 +13,8 @@ const MONGODB_CONNECTION = require("./database/database");
 //Routes
 const authRoute = require("./routes/auth.route");
 const userRoute = require("./routes/user.route");
+const productsRoute = require("./routes/products.route");
+const reviewsRoute = require("./routes/reviews.route");
 
 //Middleware
 const notFoundMiddleware = require("./middleware/not-found");
@@ -23,6 +26,8 @@ app.use(cookieParser(process.env.JWT_SECRET));
 
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/users", userRoute);
+app.use("/api/v1/products", productsRoute);
+app.use("/api/v1/reviews", reviewsRoute);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);

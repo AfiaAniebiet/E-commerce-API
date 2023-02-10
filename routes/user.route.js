240151c100768a0991fee58d23264ba8d53aa964
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-02-10 13:52:01 +0100

    Updated user route with authentication and authorization

--- routes/user.route.js
+++ routes/user.route.js
@@ -5,9 +5,9 @@ const userController = require("../controllers/user.controller");
 const { authUser, authorizePermissions } = require("../middleware/authentication");
 
 router.route("/").get(authUser, authorizePermissions("admin", "owner"), userController.getAllUsers);
-router.route("/currentUser").get(authUser, userController.showCurrentUser);
-router.route("/updateUser").patch(authUser, userController.updateUser);
-router.route("/updatePassword").patch(authUser, userController.updateUserPassword);
+router.route("/currentuser").get(authUser, userController.showCurrentUser);
+router.route("/updateuser").patch(authUser, userController.updateUser);
+router.route("/updatepassword").patch(authUser, userController.updateUserPassword);
 router.route("/:id").get(authUser, userController.getSingleUser);
 
 module.exports = router;
